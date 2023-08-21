7c59e50ef0c31edd31285a45169df0020ea53135
Umair Farooq <umair.farooq@osf-commerce.com>
2023-08-21 17:12:39 +0500

    Code splitting by lazy loading components

--- code-splitting/src/App.jsx
+++ code-splitting/src/App.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,6 +23,11 @@ const App = () => {
       <BrowserRouter>
         <AdoptedPetContext.Provider value={adoptedPet}>
           <QueryClientProvider client={queryClient}>
+          <Suspense fallback={
+            <div className="loading-pane">
+                <h2 className="loader">Loading</h2>
+            </div>
+        }>
             <header>
               <Link to="/">Adopt Me!</Link>
             </header>
@@ -29,6 +35,7 @@ const App = () => {
               <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />} />
             </Routes>
+          </Suspense>
           </QueryClientProvider>
         </AdoptedPetContext.Provider>
       </BrowserRouter>
